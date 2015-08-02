af2baeefe418adf469261e24c7d4ead9d56011cc
nycucumber <yang.wang.dec@gmail.com>
2015-08-01 23:58:42 -0400

    first commit

---
+++ custom.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+
+	$('#newstitleimage img').css('display', 'none');
+
+
+
+	$('#news #row-1 #titles #nyt').hover(
+		function () {
+			$('#row-1 #newstitleimage #nyt').css('display', 'block');
+		},
+		function () {
+			$('#row-1 #newstitleimage #nyt').css('display', 'none');
+		})
+
+
+	$('#news #row-1 #titles #tyt').hover(
+		function () {
+			$('#row-1 #newstitleimage #tyt').css('display', 'block');
+		},
+		function () {
+			$('#row-1 #newstitleimage #tyt').css('display', 'none');
+		})
+
+
+
+	$('#news #row-2 #titles #nyt').hover(
+		function () {
+			$('#row-2 #newstitleimage #nyt').css('display', 'block');
+		},
+		function () {
+			$('#row-2 #newstitleimage #nyt').css('display', 'none');
+		})
+
+
+	$('#news #row-2 #titles #tyt').hover(
+		function () {
+			$('#row-2 #newstitleimage #tyt').css('display', 'block');
+		},
+		function () {
+			$('#row-2 #newstitleimage #tyt').css('display', 'none');
+		})
+
+
+	$('#featured-projects > div > div').hover(function () {
+		$(this).css('color','#f63d12');
+		var theimage = $(this).find('img');
+		var overlay = $("<div>", {id: "foo", class: "a"});
+		overlay.css({
+			'background':'#f63d12',
+			'width':theimage.width(),
+			'height':theimage.height(),
+			'position':'absolute',
+			'top':'0',
+			'left':'15px',
+			'z-index':1000,
+			'opacity':0.2
+		})
+		
+		$(this).append(overlay);
+	}, function () {
+		$(this).css('color','#000');
+//		$(this).find('img').removeClass('tint');
+		$('#foo').remove();
+	});
+
+
+});
